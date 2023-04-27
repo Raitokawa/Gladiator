c2519582fe8cd6bb4ea9208f72df48d7832fb6c2
Raitokawa <agfa1508@gmail.com>
2023-04-28 03:00:26 +0700

    add animator update player movement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private CharacterController character;
     [SerializeField]
     private FixButtonMovement fixButton;
-    private Rigidbody rb;
     public Joystick joystick;
     [SerializeField]
-    private float moveSpeed;
+    private float ySpeed;
     [SerializeField]
-    private float jumpForce;
+    private float gravityModifier;
     [SerializeField]
     private bool isGrounded;
     [SerializeField]
@@ -21,24 +21,93 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float groundCheckRadius;
     public LayerMask groundCheckLayers;
-
-
+    private float? lastGroundedTime;
+    private float? jumpPressTime;
+    public Animator animator;
+    private bool isIdle;
+    private bool isJump;
+    private bool isFalling;
+    public float jumpPeriod;
+    public float jumpHeight;
+    public float rotationSpeed;
+    public float jumpHorizontalSpeed;
     void Start()
     {
         fixButton = FindObjectOfType<FixButtonMovement>();
         joystick = FindObjectOfType<Joystick>();
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundChecker.position,groundCheckRadius,groundCheckLayers);
-        rb.velocity = new Vector3(joystick.Horizontal * moveSpeed, rb.velocity.y, joystick.Vertical * moveSpeed);
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
 
-        if(isGrounded&& fixButton.isPressing)
+        isGrounded = Physics.CheckSphere(groundChecker.position,groundCheckRadius,groundCheckLayers);
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(moveDirection.magnitude);
+        moveDirection.Normalize();
+        float gravity = Physics.gravity.y * gravityModifier;
+        if (isJump && ySpeed > 0 && fixButton.isPressing == false)
         {
-            rb.velocity += Vector3.up * jumpForce;
+            gravity *= 2;
         }
+
+        ySpeed += gravity * Time.deltaTime;
+
+        if (character.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
+        if (fixButton.isPressing)
+        {
+            jumpPressTime = Time.time;
+        }
+        if (Time.time - lastGroundedTime <= jumpPeriod)
+        {
+            ySpeed = -0.5f;
+            isGrounded = true;
+            isJump = false;
+            isFalling = false;
+
+            if (Time.time - jumpPressTime <= jumpPeriod)
+            {
+                ySpeed = Mathf.Sqrt(jumpHeight * -3 * gravity);
+                isJump = true;
+                jumpPressTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            isGrounded = false;
+            if ((isJump && ySpeed < 0) || ySpeed < -2)
+            {
+                isFalling = true;
+            }
+        }
+        if (moveDirection != Vector3.zero)
+        {
+            isIdle = false;
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            isIdle = true;
+        }
+
+        if (isGrounded == false)
+        {
+            Vector3 velocity = moveDirection * inputMagnitude * jumpHorizontalSpeed;
+            velocity.y = ySpeed;
+            character.Move(velocity * Time.deltaTime);
+        }
+        animator.SetFloat("MoveSpeed", inputMagnitude);
+        animator.SetBool("Idle", isIdle);
+        animator.SetBool("Jump",isJump);
+        animator.SetBool("Falling",isFalling);
     }
+
 }
